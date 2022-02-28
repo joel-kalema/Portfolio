1375a957f15f4596ead2227d2367d9d2e47fa018
joel <joelkalema63@gmail.com>
2022-02-28 14:21:01 +0200

    Added the mobile menu

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', function(){
     var header = document.querySelector('header');
     header.classList.toggle('stiky',window.scrollY >0);
 })
+
+const button = document.querySelector('.button');
+const activeNav = document.querySelector('.nav-links')
+
+button.addEventListener('click', () => {
+    activeNav.classList.toggle('nav-active');
+  });
