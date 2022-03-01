a94c70411da3c28377bd7593a63f9192da1b97ca
joel <joelkalema63@gmail.com>
2022-03-01 11:56:30 +0200

    Added the style lint

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-window.addEventListener('scroll', function(){
-    var header = document.querySelector('header');
-    header.classList.toggle('stiky',window.scrollY >0);
-})
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  header.classList.toggle('stiky', window.scrollY > 0);
+});
 
-const navSlide= () => {
-    const button = document.querySelectorAll('.button');
-const activeNav = document.querySelector('.nav-links')
-const remouve = document.querySelectorAll('.link');
-
-button.forEach(btn => {
+const navSlide = () => {
+  const button = document.querySelectorAll('.button');
+  const activeNav = document.querySelector('.nav-links');
+  const remove = document.querySelectorAll('.link');
+  button.forEach((btn) => {
     btn.addEventListener('click', () => {
-        activeNav.classList.toggle('nav-active');
-      });
-});
-
-remouve.forEach(remv =>{
-    remv.addEventListener('click', () =>{
-        activeNav.classList.remove('nav-active');
+      activeNav.classList.toggle('nav-active');
     });
-});
-
-}
+  });
+  remove.forEach((remv) => {
+    remv.addEventListener('click', () => {
+      activeNav.classList.remove('nav-active');
+    });
+  });
+};
 
 navSlide();
